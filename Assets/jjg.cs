69ca4f184aa83d6f1c83575a39421f148edffec1
slink <QMBFXSL@gmail.com>
2020-02-13 03:37:03 +1300

    tha game
    hehehrh

---
+++ Assets/jjg.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jjg : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if(anim.GetBool("movementkeyspressed") == false)
+            {
+                anim.SetBool("movementkeyspressed", true);
+            }
+            else
+            {
+                anim.SetBool("movementkeyspressed", false);
+            }
+        }
+    }
+}
